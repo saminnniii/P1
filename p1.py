889615291cb2e573b348d0e1d32847d3cfc77e23
samin <samin.tmr94@gmail.com>
2026-08-13 04:24:48 +0430

    init

--- p1.py
+++ p1.py
@@ -1,13 +1,12 @@
 class User:
     def __init__(self,id,name,email):
-        self.cart=[]
         self._id=id
         self.name=name
         self.email=email
     def add_to_cart(self,product):
         self.cart.append(product)
     def remove_from_cart(self,product):
-        if product in self.cart:
+        if product in self.cart
             self.cart.remove(product)
         else:
             print("product is not in cart")
@@ -32,10 +31,66 @@ class Product:
         self.stock-=amount
 class Cart:
     def __init__(self):
-        self.cart=[]
-        
+        self.cart={}
+    def add_product(self,product,quantity):
+        if product in self.cart:
+            quantity+=self.cart[product]
+        else:
+            self.cart[product]=quantity
+    def remove_product(self,product):
+        if product in self.cart:
+            del self.cart[product]  
+        else:
+            print("this isnt in tour cart")
+    def calculate_total(self):
+        total=0  
+        for product,quantity in self.cart,items():
+            product.price*quantity=+total      
+        return total
+    def __len__(self):
+        return len(self.cart)
+    def __str__(self):
+        result=""
+        for product,quantity in self.cart.items():
+            result+=product.name +":"+ str(quantity)+ "\n"
+        return result
+    def __contains__(self,product):
+        if product in self.cart:
+            return True
+        else:
+            return False
+class Order:
+    def __init__(self,id,user,items,total_price):
+        self.id=id
+        self.user=user
+        self.items=items
+        self.total_price=total_price
+        self.status=pending
+    def pay(self,gateway):
+        if self.status=="cancelled":
+            return
+        gateway.pay(self.total_price)
+        self.status="paid"
+    def cancel(self):
+        if self.status=="paid":
+            return
+        else:
+            self.status="cancelled"
+from abc import ABC,abstractmethod
+class Paymentgateway(ABC):
+    @abstractmethod
+    def pay(self,amount):
+        pass
+class Fakepaymentgateway(Paymentgateway):
+    def pay(self,amount):
+        return "fake payment successful"
+class Bankpaymentgateway(Paymentgateway):
+    def pay(self,amount):
+        return "bank payment successful"
 
-print(p)
+
+
+
 
 
 
